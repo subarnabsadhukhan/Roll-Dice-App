7f0d8594da5f6859f1c22ef14cd399508148a4c2
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-16 19:45:59 +0530

    Alternate Shortcut: to add constructor function
    Here, {super.key} will accept a named argument called key and automatically forward it to the superclass (i.e., StatelessWidget), under that same name (i.e., key).

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 }
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({key}) : super(key: key);
+  GradientContainer({super.key});
 
   @override
   Widget build(context) {
