29d4d5341b74dfb451f22de073cd59a246f3c669
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-16 08:09:18 +0530

    change gradinet colors and add gradinet begin and end arguments

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,11 @@ void main() {
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 43, 19, 92),
+                Color.fromARGB(255, 66, 25, 146),
                 Color.fromARGB(255, 13, 16, 78),
               ],
+              begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
             ),
           ),
           child: const Center(
