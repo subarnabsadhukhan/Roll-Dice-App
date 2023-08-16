9b0295ea45517a518ec2508e6342a2c4444b9bf7
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-16 07:42:45 +0530

    change single background color to gradient & change Text style
    - delete previous backgroundColor argument.
    - wrap the center with container widget.
    - after multi-level widgets inside container, add gradient color
    - change TextStyle (color, fontSize, fontWeight)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 95, 164, 161),
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 43, 19, 92),
+                Color.fromARGB(255, 13, 16, 78),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 40,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
         ),
       ),
     ),
