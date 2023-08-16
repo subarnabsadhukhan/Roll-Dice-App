6093c903be212adcf1dffe9d8e19110874786e1f
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-16 19:20:32 +0530

    build custom widget: GradientContainer

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,36 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 66, 25, 146),
-                Color.fromARGB(255, 13, 16, 78),
-              ],
-              begin: Alignment.bottomRight,
-              end: Alignment.topLeft,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontSize: 40,
-                fontWeight: FontWeight.w800,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
 }
 
-class GradientContainer extends StatelessWidget {}
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 66, 25, 146),
+            Color.fromARGB(255, 13, 16, 78),
+          ],
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontSize: 40,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+      ),
+    );
+  }
+}
