9a69c650e0decd429e8348500c76aa500dd19063
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-17 09:30:31 +0530

    Revert "use Alternate Constructor Function (for learning only)"
    This reverts commit ffaad4344b31969d270a57c81c49bb4f16750680.

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,13 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer
-            .alternateChoice(), // use the alternate constructor function
+        body: GradientContainer(
+          [
+            Color.fromARGB(255, 66, 25, 146),
+            Color.fromARGB(255, 13, 16, 78),
+          ],
+          'Hello World!',
+        ),
       ),
     ),
   );
