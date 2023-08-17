f39b269c057f2e050b28b40f7db5cac5219d70b6
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-08-17 17:01:22 +0530

    add spacing between image and text (version 2)
    - add SizedBox between Image and TextButton
    - replace this with version 1 code

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -26,12 +26,12 @@ class GradientContainer extends StatelessWidget {
               'assets/images/dice-1.png',
               width: 200,
             ),
+            const SizedBox(
+              height: 20,
+            ),
             TextButton(
               onPressed: () {}, // Anonymous Function
               style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                ),
                 foregroundColor: Colors.white,
                 textStyle: const TextStyle(
                   fontSize: 30,
